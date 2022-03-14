1694c1fa7100980513440cf2347f376d796a2b83
Yaru Wang <yaru@interchain.io>
2022-03-14 18:37:41 +0100

    allow more ica msg types

--- app/app.go
+++ app/app.go
@@ -638,7 +638,34 @@ func NewGaiaApp(
 			// create ICS27 Host submodule params
 			hostParams := icahosttypes.Params{
 				HostEnabled:   true,
-				AllowMessages: []string{"/cosmos.bank.v1beta1.MsgSend"},
+				AllowMessages: []string{
+					"/cosmos.authz.v1beta1.MsgExec",
+					"/cosmos.authz.v1beta1.MsgGrant",
+					"/cosmos.authz.v1beta1.MsgRevoke",
+					"/cosmos.bank.v1beta1.MsgSend",
+					"/cosmos.bank.v1beta1.MsgMultiSend",
+					"/cosmos.distribution.v1beta1.MsgSetWithdrawAddress",
+					"/cosmos.distribution.v1beta1.MsgWithdrawValidatorCommission",
+					"/cosmos.distribution.v1beta1.MsgFundCommunityPool",
+					"/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward",
+					"/cosmos.feegrant.v1beta1.MsgGrantAllowance",
+					"/cosmos.feegrant.v1beta1.MsgRevokeAllowance",
+					"/cosmos.gov.v1beta1.MsgVoteWeighted",
+					"/cosmos.gov.v1beta1.MsgSubmitProposal",
+					"/cosmos.gov.v1beta1.MsgDeposit",
+					"/cosmos.gov.v1beta1.MsgVote",
+					"/cosmos.staking.v1beta1.MsgEditValidator",
+					"/cosmos.staking.v1beta1.MsgDelegate",
+					"/cosmos.staking.v1beta1.MsgUndelegate",
+					"/cosmos.staking.v1beta1.MsgBeginRedelegate",
+					"/cosmos.staking.v1beta1.MsgCreateValidator",
+					"/cosmos.vesting.v1beta1.MsgCreateVestingAccount",
+					"/ibc.applications.transfer.v1.MsgTransfer",
+					"/tendermint.liquidity.v1beta1.MsgCreatePool",
+					"/tendermint.liquidity.v1beta1.MsgSwapWithinBatch",
+					"/tendermint.liquidity.v1beta1.MsgDepositWithinBatch",
+					"/tendermint.liquidity.v1beta1.MsgWithdrawWithinBatch",
+				},
 			}
 
 			ctx.Logger().Info("start to init interchainaccount module...")
